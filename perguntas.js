3e8c7da20ca177f7795658462981235c5df1a337
ithalo213 <130056500+ithalo213@users.noreply.github.com>
2024-11-05 08:17:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'futebol',
+    'qual clube europeu tem mais títulos da Liga dos Campeões da UEFA?',
+    'o clube com mais títulos é o Real Madrid com 15 títulos'
+)
+
+criaCartao(
+    'corrida',
+    'quem é o brasileiro com mais vítoria na Fómula 1?',
+    'Ayrton Senna com 41 vitória'
+)
+
+criaCartao(
+    'jogos eletronicos',
+    'Qual console é cosiderado o mais vendido da hitória?',
+    'Playtation 2, com mais de 155 milhões de inidades vendidas'
+)
+
+criaCartao(
+    'Lingua polonês',
+    'Como se diz EU TE AMO em polonễs?',
+    'EU TE AMO em polonês é Kocham cię'
+)
+
+criaCartao(
+    'geografia',
+    'qula a capital da Belgica?',
+    'a capital da belgica é Bruxelas.'
+)
+criaCartao(
+    'Moda',
+    'em que ano foi fundado a NIKE?',
+    '25 de janeiro de 1964'
+)
